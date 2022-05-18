9c236f5478011a0c5fff4944cc77636f75962eca
vicentiu-mincior <vicentiu.mincior@consolight.ro>
2022-05-18 14:21:33 +0300

    instalare Hello world vue.js frontend

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/{any}', 'App\Http\Controllers\PagesController@index')->where('any', '.*');
